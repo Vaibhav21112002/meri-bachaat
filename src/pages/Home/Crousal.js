0436176cd2dc224ada3ce16a5c379961b4e41ddb
Vaibhav Gupta <guptavaibhav432@gmail.com>
2021-11-28 23:20:53 +0530

    Carousel Fix

--- src/pages/Home/Crousal.js
+++ src/pages/Home/Crousal.js
@@ -1,28 +1,35 @@
 import React from 'react'
 import CrousalItem from './CrousalItem'
+import img1 from "../../images/1.jpeg";
 function Crousal() {
     return (
-      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" style={{position:"relative"}}>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <CrousalItem/>
+          // <CrousalItem/>
+          <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+          <ol className="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to={0} className="active" />
+            <li data-target="#carouselExampleIndicators" data-slide-to={1} />
+            <li data-target="#carouselExampleIndicators" data-slide-to={2} />
+          </ol>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img className="d-block w-100" src={img1} alt="First slide" />
+            </div>
+            <div className="carousel-item">
+            <img style={{zIndex:"100"}} className="d-block w-100" src="https://source.unsplash.com/random" alt="Second slide" />
+            </div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src="https://source.unsplash.com/random" alt="Third slide" />
+            </div>
+          </div>
+          <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true" />
+            <span className="sr-only">Next</span>
+          </a>
         </div>
-        <div class="carousel-item">
-          Bye
-        </div>
-        <div class="carousel-item">
-         Kya haal
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
     )
 }
 
--- src/pages/Home/CrousalItem.js
+++ src/pages/Home/CrousalItem.js
@@ -2,21 +2,21 @@ import React from "react";
 
 function CrousalItem({ title }) {
   return (
-    <div class="card">
+    <div className="card">
       <div
-        class="card-horizontal"
+        className="card-horizontal"
         style={{ display: "flex", flex: "1 1 auto" }}
       >
-        <div class="card-body">
-          <h4 class="card-title">Card title</h4>
-          <p class="card-text">
+        <div className="card-body">
+          <h4 className="card-title">Card title</h4>
+          <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
         </div>
-        <div class="img-square-wrapper">
+        <div className="img-square-wrapper">
           <img
-            class=""
+            className=""
             src="http://via.placeholder.com/300x180"
             alt="Card image cap"
           />
